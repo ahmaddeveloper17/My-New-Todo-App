a6ae54b3af95c089133e2393ba4e386ed59590c7
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2025-01-16 18:04:54 +0500

    Add Home component and integrate it into the main App layout; update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Home from './components/Home'
 import SignIn from './components/Signin'
 import Signup from './components/Signup'
 
@@ -7,6 +8,7 @@ function App() {
         <>
         <Signup/>
         <SignIn/>
+        <Home/>
         </>
     )
 }
