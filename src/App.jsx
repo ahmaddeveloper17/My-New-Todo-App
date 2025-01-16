ba76e8a102697dc89fe63d32ee05011519c3034c
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2025-01-16 15:59:22 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SignIn from './components/Signin'
+import Signup from './components/Signup'
+
+function App() {
+    return (
+        <>
+        <Signup/>
+        <SignIn/>
+        </>
+    )
+}
+
+export default App
